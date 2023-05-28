b5c2043027ed6e1bc0c74525c4843f2b0e0bbc77
Anurag Dhote <anuragdhote392@gmail.com>
2023-05-28 20:47:19 +0530

    feat - Update and Complete Navbar with both Desktop & Mobile Authentication

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,18 +6,18 @@ import { useState, useEffect } from "react"
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
 
 const Nav = () => {
-	const isUserLoggedIn = true
+	const { data: session } = useSession()
 
 	const [providers, setProviders] = useState(null)
 	const [toggleDropDown, setToggleDropDown] = useState(false)
 
 	useEffect(() => {
-		const setProvider = async () => {
+		const setUpProviders = async () => {
 			const response = await getProviders()
 
 			setProviders(response)
 		}
-		setProvider()
+		setUpProviders()
 	}, [])
 
 	return (
@@ -35,13 +35,13 @@ const Nav = () => {
 
 			{/* Desktop Navigation */}
 			<div className="sm:flex hidden">
-				{isUserLoggedIn ? (
+				{session?.user ? (
 					<div className="flex gap-3 md:gap-5">
 						<Link href="/create-prompt" className="black_btn">Create Post</Link>
 						<button type="button" onClick={signOut} className="outline_btn">Sign Out</button>
 						<Link href="/profile">
 							<Image
-								src="/assets/images/logo.svg"
+								src={session?.user.image}
 								width="37"
 								height="37"
 								className="rounded-full"
@@ -53,14 +53,14 @@ const Nav = () => {
 					<>
 						{providers &&
 							Object.values(providers).map((provider) => {
-								<button
+								return (<button
 									type="button"
 									key={provider.name}
 									onClick={() => signIn(provider.id)}
 									className="black_btn"
 								>
 									Sign In
-								</button>
+								</button>)
 							})
 						}
 					</>
@@ -70,10 +70,10 @@ const Nav = () => {
 			{/* Mobile Navigation
 			 */}
 			<div className="sm:hidden flex relative">
-				{isUserLoggedIn ? (
+				{session?.user ? (
 					<div className="flex">
 						<Image
-							src="/assets/images/logo.svg"
+							src={session?.user.image}
 							width={37}
 							height={37}
 							className="rounded-full"
@@ -114,14 +114,14 @@ const Nav = () => {
 					<>
 						{providers &&
 							Object.values(providers).map((provider) => {
-								<button
+								return (<button
 									type="button"
 									key={provider.name}
 									onClick={() => signIn(provider.id)}
 									className="black_btn"
 								>
 									Sign In
-								</button>
+								</button>)
 							})
 						}
 					</>
