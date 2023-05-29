283a9c530be7927cd8fb5087284bd7d06b19eaa3
Anurag Dhote <anuragdhote392@gmail.com>
2023-05-29 18:55:08 +0530

    refactor - added htmlFor attribute to form fields

--- components/Form.jsx
+++ components/Form.jsx
@@ -14,10 +14,11 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
 				onSubmit={handleSubmit}
 				className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism"
 			>
-				<label htmlFor="">
+				<label htmlFor="prompt">
 					<span className="font-satoshi font-semibold text-base text-gray-700">Your AI Prompt</span>
 
 					<textarea 
+						id="prompt"
 						value={post.prompt}
 						onChange={(e) => setPost({ ...post, prompt: e.target.value})}
 						placeholder="Write your prompt here..."
@@ -25,12 +26,13 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
 						className="form_textarea"
 					/>
 				</label>
-				<label htmlFor="">
+				<label htmlFor="tag">
 					<span className="font-satoshi font-semibold text-base text-gray-700">Tag {' '}
 						<span className="font-normal">(#product, #webdevelopment, #idea)</span>
 					</span>
 
 					<input
+						id="tag"
 						value={post.tag}
 						onChange={(e) => setPost({ ...post, tag: e.target.value})}
 						placeholder="#tag"
